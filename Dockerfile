db1e863c7edcec7ebd6800a31ad4bc484d9e38e4
NeZlox <cnegovek@mail.ru>
2024-05-29 14:40:53 +0300

    initial

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY . .
-# запускается в докер компосе
-# RUN alembic upgrade head
-# CMD ["gunicorn", "src.main:app", "--workers", "4", "--worker-class", "uvicorn.workers.UvicornWorker", "--bind=0.0.0.0:8000"]
+
+
+
 
 
--- src/config.py
+++ src/config.py
@@ -4,9 +4,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
     LOG_LEVEL: Literal["INFO"]
 
     DB_HOST: str
+    DB_HOST_PROD: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
@@ -15,6 +17,9 @@ class Settings(BaseSettings):
 
     REDIS_HOST: str
     REDIS_PORT: int
+
+    REDIS_HOST_PROD: str
+    REDIS_PORT_PROD: int
 
 
 
@@ -28,4 +33,16 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
 
+    def post_settings(self):
+        if self.MODE == "PROD":
+            self.DB_HOST = self.DB_HOST_PROD
+
+
+            self.REDIS_HOST = self.REDIS_HOST_PROD
+            self.REDIS_PORT = self.REDIS_PORT_PROD
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
 settings = Settings()
+settings.post_settings()
