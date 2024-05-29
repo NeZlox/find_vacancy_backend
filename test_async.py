4092da892df825e7b3674d8e69e06ac0643d88e9
NeZlox <cnegovek@mail.ru>
2024-05-29 15:36:44 +0300

    initial

--- test_async.py
+++ test_async.py
@@ -3,11 +3,46 @@ import asyncio
 import threading
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
+import traceback
 
-from src.tasks.get_bs4 import get_content
+import aiohttp
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 URL = "https://volgograd.hh.ru/vacancies"
 URL = 'https://volgograd.hh.ru/vacancies/programmist?page=0'
+
+
+
+
+async def get_content(url, proxy=None) -> BeautifulSoup | None:
+    error = 429
+    cnt = 0
+
+    while error == 429 and cnt < 5:
+        cnt += 1
+        headers = {
+            "User-Agent": UserAgent().random
+        }
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, headers=headers, proxy=proxy if proxy else None,timeout=30) as response:
+                    error = response.status
+                    if response.status == 200:
+                        html = await response.text()
+                        soup = BeautifulSoup(html, 'lxml')
+                        return soup
+            await asyncio.sleep(10)
+
+        except Exception as e:
+            print(traceback.format_exc())
+            print(e)
+            print(proxy)
+            return None
+
+    return None
+
 
 
 
